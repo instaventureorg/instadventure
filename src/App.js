743bc359304da96f81a749ac76d5bbf5827ec3d9
Alex Kramer <alexkrameris@gmail.com>
2019-06-20 11:13:59 -0400

    user id NOW in app state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, NavLink } from 'react-router-dom';
-import {Button} from 'react-bootstrap'
-import { LandingPage, GamePage, GameListPage, UserPage, /*Login, Signup*/ } from './routes/routes.js'
-import './App.css';
-// import fireApp from './fire.js'
-// import firebase from 'firebase'
+import React from "react";
+import { Route, BrowserRouter as Router, NavLink } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import {
+  LandingPage,
+  GamePage,
+  GameListPage,
+  UserPage /*Login, Signup*/
+} from "./routes/routes.js";
+import "./App.css";
+import fireApp from "./fire.js";
+import firebase from "firebase";
 
-export default class App extends React.Component{
-  render (){
-  return (
-    <Router>
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userId: null
+    };
+  }
+  componentDidMount() {
+    let that = this;
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log(user);
+        that.setState({ userId: user.uid });
+      } else {
+        that.setState({ userId: null });
+      }
+    });
+  }
+  render() {
+    return (
+      <Router>
         <div className="App">
-          <nav id='nav-links' className="Links">
-            <NavLink to="/"><Button>Home</Button></NavLink>
-            <NavLink to="/user"><Button>User</Button></NavLink>
-            <NavLink to="/game_list"><Button>Game List</Button></NavLink>
+          <nav id="nav-links" className="Links">
+            <NavLink to="/">
+              <Button>Home</Button>
+            </NavLink>
+            <NavLink to="/user">
+              <Button>User</Button>
+            </NavLink>
+            <NavLink to="/game_list">
+              <Button>Game List</Button>
+            </NavLink>
             {/* <NavLink to="/game"><Button>Game</Button></NavLink> */}
           </nav>
 
@@ -29,12 +57,22 @@ export default class App extends React.Component{
           </div>
 
           <div className="Footer">
-            <p><b><span className='instadventure-name'>InstAdventure</span></b> was created for <a href='https://www.developerweek.com/NYC/'>DeveloperWeek NYC</a>'s 2019 Hackathon by Jason Bennett, Claire Gilligan, Alex Kramer, and Malhar Teli, with Blake Dignes. <a href='https://github.com/instaventureorg/instadventure'>Check out our code</a>!</p>
-
+            <p>
+              <b>
+                <span className="instadventure-name">InstAdventure</span>
+              </b>{" "}
+              was created for{" "}
+              <a href="https://www.developerweek.com/NYC/">DeveloperWeek NYC</a>
+              's 2019 Hackathon by Jason Bennett, Claire Gilligan, Alex Kramer,
+              and Malhar Teli, with Blake Dignes.{" "}
+              <a href="https://github.com/instaventureorg/instadventure">
+                Check out our code
+              </a>
+              !
+            </p>
           </div>
         </div>
       </Router>
-  )}
+    );
+  }
 }
-
-
