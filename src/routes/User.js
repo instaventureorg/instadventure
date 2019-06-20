83d5470d01a91fefa604cb28c68b75a89e06a126
malharteli <malhar.teli@gmail.com>
2019-06-20 09:05:10 -0400

    added userid to the state, which is not actually necessary, but I'm still trying to figure out how to make it work

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import fireApp from '../fire';
+import firebase from 'firebase'
 
 export default class UserPage extends React.Component {
+  constructor(props){
+    super()
+    this.state ={
+      dbUser:null,
+      user: ""
+    }
+    this.docRef = firebase.auth().onAuthStateChanged((user)=>{
+      this.setState({user:user.uid})
+    })
+    this.ref = {}
+  }
+
+  componentDidMount(){
+    if (this.state.user){
+    if (fireApp.firestore().collection('users').doc(this.state.user)){
+      console.log(this.state.user)
+    }}
+  }
+
   render() {
-    return (<p>User Page </p>)
+    return (<div>
+      <p>User Page</p>
+      <p>{}</p>
+      </div>)
   }
 }
